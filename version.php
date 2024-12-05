16ace1415633b28d6ded528e563502284577f176
Hamza Hummam <hamzahummam16@gmail.com>
2024-12-05 11:35:31 +0500

    Update version.php

--- version.php
+++ version.php
@@ -4,4 +4,4 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2024110601;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2020110300;        // Requires this Moodle version
 $plugin->component = 'block_courses_by_month'; // Plugin name
-$plugin->requires  = 1.0; // Plugin release
+$plugin->release  = 1.0; // Plugin release
